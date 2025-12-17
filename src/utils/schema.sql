8c4e17480ce848d9cd188c74f0880d17713c353d
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-17 20:08:05 +0300

    user model is improved

--- src/utils/schema.sql
+++ src/utils/schema.sql
@@ -3,7 +3,7 @@
       full_name VARCHAR(150) NOT NULL,
       email VARCHAR(150) UNIQUE NOT NULL,
       password_hash TEXT NOT NULL,
-      role VARCHAR(20) NOT NULL CHECK (role IN ('ADMIN', 'HR', 'INTERN')),
+      role VARCHAR(20) NOT NULL CHECK (role IN ('ADMIN', 'HR', 'STAFF')),
       is_active BOOLEAN DEFAULT true,
       last_login_at TIMESTAMP NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,

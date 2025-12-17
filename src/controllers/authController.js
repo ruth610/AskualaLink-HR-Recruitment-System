91e60bca49063c0e60bb257b2c6e071730747a58
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-18 00:03:20 +0300

    code cleanness

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -38,14 +38,23 @@ async function login(req,res){
 
     try {
         if (!email) {
-            return res.status(statusCode.BAD_REQUEST).json({ message: 'Email is required' });
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Email is required'
+            });
         }
         if (!password) {
-            return res.status(statusCode.BAD_REQUEST).json({ message: 'Password is required' });
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Password is required'
+            });
         }
         const result = await authService.login(email, password);
         if (result.status !== statusCode.OK) {
-            return res.status(result.status).json({ message: result.message });
+            return res.status(
+                result.status).json({
+                message: result.message
+            });
         }
         payload = {
             userId: result.data.id,
@@ -54,12 +63,24 @@ async function login(req,res){
             role: result.data.role
         }
         const jwt_secret = process.env.JWT_SECRET;
-        const token = jwt.sign(payload, jwt_secret, { expiresIn: '24h' });
-        return res.status(result.status).json({ message: result.message, data: { token } });
+        const token = jwt.sign(
+            payload, jwt_secret,
+            {
+                expiresIn: '24h'
+            }
+        );
+        return res.status(
+            result.status).json({
+            message: result.message,
+            data: { token }
+        });
 
     } catch (error) {
         console.error('Error during login:', error);
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+        return res.status(
+            statusCode.INTERNAL_SERVER_ERROR).json({
+            message: 'Internal server error'
+        });
     }
 
 }
@@ -92,28 +113,57 @@ async function createUser(req, res){
         const { fullName, password, email, role } = req.body;
         // validate input
         if (!fullName) {
-            return res.status(statusCode.BAD_REQUEST).json({ message: 'Fullname is required' });
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Fullname is required'
+            });
         }
         if (!password) {
-            return res.status(statusCode.BAD_REQUEST).json({ message: 'Password is required' });
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Password is required'
+            });
         }
         if (!email) {
-            return res.status(statusCode.BAD_REQUEST).json({ message: 'Email is required' });
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Email is required'
+            });
         }
         if (!role) {
             role = 'STAFF';
         }
+        if(password.length < 8){
+            return res.status(
+                statusCode.BAD_REQUEST).json({
+                message: 'Password must be at least 8 characters long'
+            });
+        }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         // call service to create user
-        const result = await authService.createUser({ fullName, password: hashedPassword, email, role });
+        const result = await authService.createUser({
+            fullName,
+            password: hashedPassword,
+            email, role
+        });
         if (result.status !== statusCode.CREATED) {
-            return res.status(result.status).json({ message: result.message });
+            return res.status(
+                result.status).json({
+                message: result.message
+            });
         }
-        return res.status(result.status).json({ message: result.message, data: result.data });
+        return res.status(
+            result.status).json({
+            message: result.message,
+            data: result.data
+        });
     } catch (error) {
         console.error('Error during user creation:', error);
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+        return res.status(
+            statusCode.INTERNAL_SERVER_ERROR).json({
+            message: 'Internal server error' }
+        );
     }
 }
 

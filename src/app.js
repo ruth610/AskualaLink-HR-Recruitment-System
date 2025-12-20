377d7b57b23b642a8054354a2c0a723a5b58d7e9
Ruth Ambaw <ruthambaw610@gmail.com>
2025-12-20 19:58:59 +0300

    error catching logic for the multer

--- src/app.js
+++ src/app.js
@@ -11,4 +11,25 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/auth', authRoutes.router);
 
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      return res.status(400).json({
+        success: false,
+        message: 'File is too large. Max limit is 5MB.'
+      });
+    }
+    return res.status(400).json({ success: false, message: err.message });
+  }
+
+  if (err.message === "Only .pdf, .doc and .docx formats allowed!") {
+    return res.status(400).json({
+      success: false,
+      message: err.message
+    });
+  }
+
+  return res.status(500).json({ success: false, message: 'Internal Server Error'});
+});
+
 export default app;
